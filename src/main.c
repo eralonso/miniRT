c4e841731946f74ba79352d975bd7388349ece23
Éric Alonso Martínez <113835039+eralonso@users.noreply.github.com>
2023-08-01 20:10:28 +0200

    ft_isdouble: Checking function do_state

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 16:36:40 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/08/01 19:28:16 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/08/01 19:50:23 by eralonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,65 +15,65 @@
 
 int	main(int argc, char const *argv[])
 {
-	t_minirt_data	*minirt;
-	t_matrix		m;
-	double			**_m;
-	t_vector		a;
-	double			*_a;
-	double			*_b;
-	t_vector		res;
+	// t_minirt_data	*minirt;
+	// t_matrix		m;
+	// double			**_m;
+	// t_vector		a;
+	// double			*_a;
+	// double			*_b;
+	// t_vector		res;
 
 	if (argc == 2)
 		printf("%d %30.28G\n", \
 					ft_validate_double((char *)argv[1]), ft_strtod(argv[1]));
 	else
 		printf("use argment\n");
-	a[0] = 1.0;
-	a[1] = 0.0;
-	a[2] = 0.0;
-	_a = ft_vector_create(a);
-	_b = ft_vector_create(_a);
-	_b[0] = 0.0;
-	_b[1] = 1.0;
-	_b[2] = 0.0;
-	m[0] = ft_vector_create(_a);
-	ft_scale_vector(m[0], m[0], 0.25);
-	m[1] = ft_vector_create(_b);
-	ft_scale_vector(m[1], m[1], 4.0);
-	m[2] = ft_vector_create(_b);
-	m[2] = ft_cross_product(m[2], m[0], m[1]);
-	ft_scale_vector(res, _a, 1.0);
-	ft_addition(res, res, _a);
-	ft_addition(res, res, _a);
-	ft_substraction(res, res, res);
-	printf("(%20.18G, %20.18G, %20.18G)\n", res[0], res[1], res[2]);
-	ft_scale_vector(res, _a, 2.0);
-	printf("(%20.18G, %20.18G, %20.18G)\n", res[0], res[1], res[2]);
-	ft_normalize(res, _a);
-	printf("(%20.18G, %20.18G, %20.18G)\n", res[0], res[1], res[2]);
-	printf("a = (%20.18G, %20.18G, %20.18G)\n", _a[0], _a[1], _a[2]);
-	printf("b = (%20.18G, %20.18G, %20.18G)\n", _b[0], _b[1], _b[2]);
-	printf("dot product a*b %20.18G\n", ft_dot_product(_a, _b));
-	ft_cross_product(res, _a, _b);
-	printf("a^b = (%20.18G, %20.18G, %20.18G)\n", res[0], res[1], res[2]);
-	ft_cross_product(res, res, a);
-	printf("(a^b)^a = (%20.18G, %20.18G, %20.18G)\n", res[0], res[1], res[2]);
-	_m = ft_matrix_create(m);
-	printf("m[0] = (%20.18G, %20.18G, %20.18G)\n", _m[0][0], _m[0][1], _m[0][2]);
-	printf("m[1] = (%20.18G, %20.18G, %20.18G)\n", _m[1][0], _m[1][1], _m[1][2]);
-	printf("m[2] = (%20.18G, %20.18G, %20.18G)\n", _m[2][0], _m[2][1], _m[2][2]);
-	_a = ft_vector_dispose(_a);
-	_b = ft_vector_dispose(_b);
-	m[0] = ft_vector_dispose(m[0]);
-	m[1] = ft_vector_dispose(m[1]);
-	m[2] = ft_vector_dispose(m[2]);
-	_m = ft_matrix_dispose(_m);
-	return (0);
-	minirt = minirt_constructor(ft_take_argument(argc, argv));
-	if (minirt)
-		mlx_loop(minirt->gr_ctx->inst);
-	ft_putstr_fd ("exit main\n", 1);
-	minirt_dispose(&minirt);
+	// a[0] = 1.0;
+	// a[1] = 0.0;
+	// a[2] = 0.0;
+	// _a = ft_vector_create(a);
+	// _b = ft_vector_create(_a);
+	// _b[0] = 0.0;
+	// _b[1] = 1.0;
+	// _b[2] = 0.0;
+	// m[0] = ft_vector_create(_a);
+	// ft_scale_vector(m[0], m[0], 0.25);
+	// m[1] = ft_vector_create(_b);
+	// ft_scale_vector(m[1], m[1], 4.0);
+	// m[2] = ft_vector_create(_b);
+	// m[2] = ft_cross_product(m[2], m[0], m[1]);
+	// ft_scale_vector(res, _a, 1.0);
+	// ft_addition(res, res, _a);
+	// ft_addition(res, res, _a);
+	// ft_substraction(res, res, res);
+	// printf("(%20.18G, %20.18G, %20.18G)\n", res[0], res[1], res[2]);
+	// ft_scale_vector(res, _a, 2.0);
+	// printf("(%20.18G, %20.18G, %20.18G)\n", res[0], res[1], res[2]);
+	// ft_normalize(res, _a);
+	// printf("(%20.18G, %20.18G, %20.18G)\n", res[0], res[1], res[2]);
+	// printf("a = (%20.18G, %20.18G, %20.18G)\n", _a[0], _a[1], _a[2]);
+	// printf("b = (%20.18G, %20.18G, %20.18G)\n", _b[0], _b[1], _b[2]);
+	// printf("dot product a*b %20.18G\n", ft_dot_product(_a, _b));
+	// ft_cross_product(res, _a, _b);
+	// printf("a^b = (%20.18G, %20.18G, %20.18G)\n", res[0], res[1], res[2]);
+	// ft_cross_product(res, res, a);
+	// printf("(a^b)^a = (%20.18G, %20.18G, %20.18G)\n", res[0], res[1], res[2]);
+	// _m = ft_matrix_create(m);
+	// printf("m[0] = (%20.18G, %20.18G, %20.18G)\n", _m[0][0], _m[0][1], _m[0][2]);
+	// printf("m[1] = (%20.18G, %20.18G, %20.18G)\n", _m[1][0], _m[1][1], _m[1][2]);
+	// printf("m[2] = (%20.18G, %20.18G, %20.18G)\n", _m[2][0], _m[2][1], _m[2][2]);
+	// _a = ft_vector_dispose(_a);
+	// _b = ft_vector_dispose(_b);
+	// m[0] = ft_vector_dispose(m[0]);
+	// m[1] = ft_vector_dispose(m[1]);
+	// m[2] = ft_vector_dispose(m[2]);
+	// _m = ft_matrix_dispose(_m);
+	// return (0);
+	// minirt = minirt_constructor(ft_take_argument(argc, argv));
+	// if (minirt)
+	// 	mlx_loop(minirt->gr_ctx->inst);
+	// ft_putstr_fd ("exit main\n", 1);
+	// minirt_dispose(&minirt);
 	return (0);
 }
 
--- src_common/utils/ft_isdouble.c
+++ src_common/utils/ft_isdouble.c
@@ -6,7 +6,7 @@
 /*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 12:45:42 by eralonso          #+#    #+#             */
-/*   Updated: 2023/08/01 13:45:06 by eralonso         ###   ########.fr       */
+/*   Updated: 2023/08/01 20:09:56 by eralonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static char	*ft_purge_chars(char *str, const char *set)
 {
 	int	i;
 
-	while (*str) // Cambio -> Antes: while (str) | Después: while (*str).
+	while (*str)
 	{
 		i = 0;
 		while (set[i] && *str != set[i])
@@ -30,207 +30,71 @@ static char	*ft_purge_chars(char *str, const char *set)
 	return (str);
 }
 
+static int	check_after_num(char *str, int state, int dot)
+{
+	int	next_state;
+
+	next_state = 0;
+	if (!*str)
+		next_state = -state;
+	else if (dot && *str == '.' && str[1] != '\0')
+		next_state = 4;
+	else if ((*str == 'e' || *str == 'E') && str[1] != '\0')
+		next_state = 5;
+	return (next_state);
+}
+
+static char	*do_state(char *str, int *state, char *set)
+{
+	char	*ret;
+	int		next_state;
+
+	ret = ft_purge_chars(str, set);
+	next_state = 0;
+	if (*state == 1 && *ret)
+		next_state = 2;
+	else if (*state == 2 && ret - str <= 1 && *ret)
+		next_state = 3;
+	else if (*state == 3)
+		next_state = check_after_num(ret++, *state, 1);
+	else if (*state == 4)
+		next_state = check_after_num(ret++, *state, 0);
+	else if (*state == 5)
+		next_state = 6 * (ret - str <= 1 && *ret);
+	else if (*state == 6)
+		next_state = -(*state) * (ret == str && *ret);
+	*state = next_state;
+	return (ret);
+}
+
 int	ft_validate_double(char *str)
 {
 	int		state;
-	char	*aux;
 
 	state = 1;
 	while (*str && state > 0)
 	{
 		if (state == 1)
-		{
-			str = ft_purge_chars(str, " ");
-			if (!*str)
-				return (0);
-			state = 2;
-		}
+			str = do_state(str, &state, " ");
 		else if (state == 2)
-		{
-			aux = str;
-			str = ft_purge_chars(str, "+-");
-			if (str - aux > 1 || !*str)
-				return (0);
-			state = 3;
-		}
+			str = do_state(str, &state, "+-");
 		else if (state == 3)
-		{
-			str = ft_purge_chars(str, "0123456789");
-			if (!*str)
-				return (1);
-			else if (*str == '.')
-				state = 4;
-			else if ((*str == 'e' || *str == 'E') && str[1] != '\0')
-				state = 5;
-			else
-				return (0);
-			str++;
-		}
+			str = do_state(str, &state, "0123456789");
 		else if (state == 4)
-		{
-			str = ft_purge_chars(str, "0123456789");
-			if (!*str)
-				return (1);
-			else if ((*str == 'e' || *str == 'E') && str[1] != '\0')
-				state = 5;
-			else
-				return (0);
-			str++;
-		}
+			str = do_state(str, &state, "0123456789");
 		else if (state == 5)
-		{
-			aux = str;
-			str = ft_purge_chars(str, "+-");
-			if (str - aux > 1 || !*str)
-				return (0);
-			state = 6;
-		}
+			str = do_state(str, &state, "+-");
 		else if (state == 6)
-		{
-			aux = str;
-			str = ft_purge_chars(str, "0123456789");
-			return (str > aux && !*str);
-		}
+			str = do_state(str, &state, "0123456789");
 		else
-			return (0);
+			state = 0;
 	}
 	return (-state);
 }
 
-// int	ft_validate_double(char *str)
-// {
-// 	int		state;
-// 	char	*aux;
-
-// 	state = 0;
-// 	while (*str) // Cambio -> Antes: while (str) | Después: while (*str).
-// 	{
-// 		if (state == 0)
-// 		{
-// 			str = ft_purge_chars(str, " ");
-// 			if (!*str) // Cambio -> Comprobar que no hay solo espacios.
-// 				return (0);
-// 			state = 1;
-// 		}
-// 		else if (state == 1)
-// 		{
-// 			aux = str;
-// 			str = ft_purge_chars(str, "+-");
-// 			if (str - aux > 1 || !*str) // Cambio -> Comprobar que el número no sea simplemente un signo ("   +", "-", ...).
-// 				return (0);
-// 			state = 2;
-// 		}
-// 		else if (state == 2)
-// 		{
-// 			str = ft_purge_chars(str, "0123456789");
-// 			// if (!*str)
-// 			// 	return (1);
-// 			// else if (*str == '.')
-// 			// {
-// 			// 	str++;
-// 			// 	state = 3;
-// 			// }
-// 			// else if (*str == 'e' || *str == 'E')
-// 			// {
-// 			// 	str++;
-// 			// 	state = 4;
-// 			// }
-// 			// else
-// 			// 	return (0);
-// 			// Cambio -> Ya que si el carácter no es ni '.', ni 'e' o 'E', salimos de la función, podemos hacer la iteración de la string después de los condicionales.
-// 			if (!*str)
-// 				return (1);
-// 			else if (*str == '.')
-// 				state = 3;
-// 			else if (*str == 'e' || *str == 'E')
-// 				state = 4;
-// 			else
-// 				return (0);
-// 			str++;
-// 		}
-// 		else if (state == 3)
-// 		{
-// 			str = ft_purge_chars(str, "0123456789");
-// 			// if (!*str)
-// 			// 	return (1);
-// 			// else if (*str == 'e' || *str == 'E')
-// 			// {
-// 			// 	str++;
-// 			// 	state = 4;
-// 			// }
-// 			// else
-// 			// 	return (0);
-// 			// Cambio -> Lo mismo que el caso anterior, itermaos después de los condicionales.
-// 			if (!*str)
-// 				return (1);
-// 			else if (*str == 'e' || *str == 'E')
-// 				state = 4;
-// 			else
-// 				return (0);
-// 			str++;
-// 		}
-// 		else if (state == 4)
-// 		{
-// 			aux = str;
-// 			str = ft_purge_chars(str, "+-");
-// 			if (str - aux > 1 || !*str) // Cambio -> Comprobar que después de la e/E no haya simplemente un símbolo ("-1e+", "3.1415926535e-").
-// 				return (0);
-// 			state = 5;
-// 		}
-// 		else if (state == 5)
-// 		{
-// 			aux = str;
-// 			str = ft_purge_chars(str, "0123456789");
-// 			return ((str > aux));
-// 		}
-// 		else
-// 			return (0);
-// 	}
-// 	return (0);
-// }
-
-// static int	ft_check_dots(char *str)
-// {
-// 	int	i;
-// 	int	dot_cnt;
-// 	int	e_cnt;
-
-// 	i = 0;
-// 	dot_cnt = 0;
-// 	e_cnt = 0;
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '.')
-// 		{
-// 			if (e_cnt > 0)
-// 				return (0);
-// 			dot_cnt++;
-// 		}
-// 		if (str[i] == 'e' || str[i] == 'E')
-// 		{
-// 			e_cnt++;
-// 			ft_check_signs(str+i+1, &i);
-// 		}
-// 		if (dot_cnt > 1 || e_cnt > 1)
-// 			return (0);
-// 		i++;
-// 	}
-// 	return (1);
-// }
-
-// static int	ft_check_signs(char *str, int *i)
-// {
-// 	if (!str || (ft_strchr("+-", str[0]) && ft_strchr("+-", str[1])))
-// 		return (0);
-// 	if (ft_strchr("+-", str[0]))
-// 		(*i)++;
-// 	return (1);
-// }
-
 int	ft_isdouble(char *str, double *ret)
 {
 	int		i;
-	// int		exp_phase;
 
 	*ret = ft_strtod(str);
 	i = 0;
@@ -238,17 +102,5 @@ int	ft_isdouble(char *str, double *ret)
 		return (0);
 	if (!ft_validate_double(str))
 		return (0);
-	// if (!ft_check_signs(str, &i) || !ft_check_dots(str))
-	// 	return (0);
-	// if (!str[i])
-	// 	return (0);
-	// while (str[i])
-	// {
-	// 	if (!(ft_isdigit(str[i]) || str[i] == '.' \
-	// 		|| str[i] == 'e' || str[i] == 'E' \
-	// 		|| str[i] == '+' || str[i] == '-'))
-	// 		return (0);
-	// 	i++;
-	// }
 	return (1);
 }
