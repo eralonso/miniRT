a0e5220c5fd18d4b12fefd68e04a7d0717b85605
Oscar Moreno Sancho <omoreno-@car9s2.42barcelona.com>
2023-08-22 13:22:08 +0200

    cone intersection improved

--- src_common/geometry/intersections/ft_quadrat_eq.c
+++ src_common/geometry/intersections/ft_quadrat_eq.c
@@ -6,7 +6,7 @@
 /*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 10:00:29 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/08/21 17:46:16 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/08/22 12:16:22 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,18 @@ double	ft_quadrat_eq(double coef[3], int sign)
 {
 	double	disc;
 	double	dsign;
+	double	roots[2];
 
+	if (fabs(coef[0]) < 1.0e-10)
+		return (-0.5 * coef[2] / coef[1]);
 	dsign = 1.0 * sign;
 	dsign /= fabs(dsign);
 	disc = (coef[1] * coef[1] - coef[0] * coef[2]);
 	if (disc < 0)
 		return (INFINITY); 
-	return ((-coef[1] - dsign * sqrt(disc)) / coef[0]);
+	roots[0] = (-coef[1] - sqrt(disc)) / coef[0];
+	roots[1] = (-coef[1] + sqrt(disc)) / coef[0];
+	if (roots[0] < roots[1] && sign > 0)
+		return (roots[0]);
+	return (roots[1]);
 }
--- src_common/geometry/intersections/inter_cone_line.c
+++ src_common/geometry/intersections/inter_cone_line.c
@@ -6,7 +6,7 @@
 /*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 15:37:00 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/08/21 17:59:42 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/08/22 13:11:18 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ static int	ft_coef_calc(double coef[3], t_line line, const t_cone *cone)
 	w_dot_h = ft_dot_product(w, (double *)cone->orientation);
 	coef[0] = v_dot_h * v_dot_h;
 	coef[0] -= cos_theta_sq;
+	if (fabs(coef[0]) < 1.0e-5)
+		return (0);
 	coef[1] = v_dot_h * w_dot_h;
 	coef[1] -= v_dot_w * cos_theta_sq;
 	coef[2] = w_dot_h * w_dot_h;
@@ -54,7 +56,11 @@ static int	ft_cap_inters(t_intersect_data *ret, \
 		ft_scale_vector(cap_center, (double *)cone->orientation, height));
 	cap_radius = height * tan(cone->theta);
 	ft_copy_vector(ret->tan_plane.point, cap_center);
-	ret->tan_plane.color = cone->color;
+	// ret->tan_plane.color = cone->color;
+	ret->tan_plane.color.r = 0;
+	ret->tan_plane.color.g = 0;
+	ret->tan_plane.color.b = 0;
+	ret->tan_plane.color.a = 0;
 	if (inter_plane_line(ret, line, (void *)&ret->tan_plane) == 0.0)
 		return (0);
 	axe_dist = ft_distance_sq(cap_center, ret->tan_plane.point);
@@ -74,7 +80,7 @@ static int	ft_give_inters(t_intersect_data *ret, \
 	ft_copy_vector(ret->tan_plane.point, line_int);
 	ft_substraction(int_rel_center, line_int, center);
 	ft_cross_product(ret->tan_plane.orientation, \
-		(double *)cone->orientation, int_rel_center);
+		int_rel_center, (double *)cone->orientation);
 	ft_cross_product(ret->tan_plane.orientation, \
 		ret->tan_plane.orientation, int_rel_center);
 	ft_normalize(ret->tan_plane.orientation, ret->tan_plane.orientation);
@@ -100,8 +106,5 @@ int	inter_cone_line(t_intersect_data *ret, t_line line, void *figure)
 		ft_substraction(ret->tan_plane.point, line_int, (double *)cone->point));
 	if (int_height > cone->heights[0] && int_height < cone->heights[1])
 		return (ft_give_inters(ret, line_int, cone, int_height));
-	if ((int_height * ft_dot_product(line.orientation, \
-			(double *)cone->orientation)) >= 0)
-		return (0);
 	return (ft_cap_inters(ret, line, cone));
 }
